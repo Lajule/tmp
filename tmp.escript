760fa4df58b73c440fd919a7e44dc6840ad1f362
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2023-10-08 22:01:47 +0200

    Initial commit

---
+++ tmp.escript
@@ -0,0 +1,6 @@
+#!/usr/bin/env escript
+%% -*- erlang -*-
+%%! -smp enable -sname tmp -mnesia debug verbose
+
+main(_) ->
+   io:format("tmp\n").
---
+++ tmp.hs
@@ -0,0 +1,5 @@
+#!/usr/bin/env stack
+-- stack --allow-different-user script --resolver lts-21.14 --package base
+
+main = do
+     putStrLn "tmp"
